a31065e423c355c96a939d3523ffde80edf10e3c
jinho <g1063114@naver.com>
2021-10-20 18:07:27 +0900

    dynamic programming

--- src/Test.java
+++ src/Test.java
@@ -1,63 +1,25 @@
 import java.util.*;
 
-class Edge implements Comparable<Edge>{
-    public int v1;
-    public int v2;
-    public int cost;
-
-    public Edge(int v1, int v2, int cost) {
-        this.v1 = v1;
-        this.v2 = v2;
-        this.cost = cost;
-    }
-
-    @Override
-    public int compareTo(Edge o) {
-        return this.cost - o.cost;
-    }
-}
-
 class Main {
-    static int[] arr;
-    static int answer = 0;
+    static int[] dy;
 
-    public static int Find(int v){
-        if( v == arr[v] ) return v;
-        else return arr[v] = Find(arr[v]);
-    }
+    public static int solution(int n){
 
-    public static void Union(int a, int b){
-        int fa = Find(a);
-        int fb = Find(b);
-        if( fa != fb ) arr[fa] = fb;
+        dy[1] = 1;
+        dy[2] = 2;
+
+        for( int i = 3; i <= n; i++ ){
+            dy[i] = dy[i-2] + dy[i-1];
+        }
+
+        return dy[n];
     }
 
     public static void main(String[] args) {
         Main T = new Main();
         Scanner kb = new Scanner(System.in);
         int n = kb.nextInt();
-        int m = kb.nextInt();
-        arr = new int[n+1];
-        List<Edge> temp = new ArrayList<>();
-
-        for( int i = 1; i <= n; i++ ){
-            arr[i] = i;
-        }
-        for( int i = 1 ; i <= m; i++){
-            int a = kb.nextInt();
-            int b = kb.nextInt();
-            int c = kb.nextInt();
-            temp.add(new Edge(a,b,c));
-        }
-        Collections.sort(temp);
-        for(Edge e : temp){
-            int fv1 = Find(e.v1);
-            int fv2 = Find(e.v2);
-            if( fv1 != fv2 ){
-                answer+= e.cost;
-                Union(e.v1, e.v2);
-            }
-        }
-        System.out.println(answer);
+        dy = new int[n+1];
+        System.out.println(solution(n));
     }
 }
