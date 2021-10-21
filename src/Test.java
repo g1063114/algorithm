06f8d67543c54e45d92994533a3e06810a8b442a
jinho <g1063114@naver.com>
2021-10-21 14:27:53 +0900

    String reverse

--- src/Test.java
+++ src/Test.java
@@ -1,58 +1,26 @@
 import java.util.*;
 
-class Brick implements Comparable<Brick>{
+class Main{
+    public List<String> solution(int n, String[] s){
+        List<String> answer = new ArrayList<>();
 
-    public int s;
-    public int h;
-    public int w;
-
-    public Brick(int s, int h, int w) {
-        this.s = s;
-        this.h = h;
-        this.w = w;
-    }
-
-    @Override
-    public int compareTo(Brick o) {
-        return o.s - this.s;
-    }
-}
-
-class Main {
-    static int[] dy;
-
-    public static int solution(List<Brick> arr){
-        int answer = 0 ;
-        Collections.sort(arr);
-        dy[0] = arr.get(0).h;
-        answer = dy[0];
-
-        for( int i = 1 ; i < arr.size(); i++ ){
-            int max = 0;
-            for( int j = i - 1; j >=0; j--){
-                if(arr.get(j).w > arr.get(i).w && dy[j] > max){
-                    max = dy[j];
-                }
-            }
-            dy[i] = max + arr.get(i).h;
-            answer = Math.max(answer, dy[i]);
+        for( int i = 0 ; i < n; i++ ){
+            String temp = new StringBuilder(s[i]).reverse().toString();
+            answer.add(temp);
         }
         return answer;
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args){
         Main T = new Main();
         Scanner kb = new Scanner(System.in);
         int n = kb.nextInt();
-        List<Brick> arr = new ArrayList<>();
-        dy = new int[n];
-
+        String[] arr = new String[n];
         for( int i = 0 ; i < n; i++ ){
-            int a = kb.nextInt();
-            int b = kb.nextInt();
-            int c = kb.nextInt();
-            arr.add(new Brick(a,b,c));
+            arr[i] = kb.next();
         }
-        System.out.println(solution(arr));
+        for( String x : T.solution(n,arr)){
+            System.out.println(x);
+        }
     }
 }
