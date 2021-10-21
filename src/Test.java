a92ec3ca229d83cca0eaa965e1f49bc6c77df991
jinho <g1063114@naver.com>
2021-10-21 13:15:37 +0900

    dynamic programming - LIS

--- src/Test.java
+++ src/Test.java
@@ -1,25 +1,58 @@
 import java.util.*;
 
+class Brick implements Comparable<Brick>{
+
+    public int s;
+    public int h;
+    public int w;
+
+    public Brick(int s, int h, int w) {
+        this.s = s;
+        this.h = h;
+        this.w = w;
+    }
+
+    @Override
+    public int compareTo(Brick o) {
+        return o.s - this.s;
+    }
+}
+
 class Main {
     static int[] dy;
 
-    public static int solution(int n){
+    public static int solution(List<Brick> arr){
+        int answer = 0 ;
+        Collections.sort(arr);
+        dy[0] = arr.get(0).h;
+        answer = dy[0];
 
-        dy[1] = 1;
-        dy[2] = 2;
-
-        for( int i = 3; i <= n; i++ ){
-            dy[i] = dy[i-2] + dy[i-1];
+        for( int i = 1 ; i < arr.size(); i++ ){
+            int max = 0;
+            for( int j = i - 1; j >=0; j--){
+                if(arr.get(j).w > arr.get(i).w && dy[j] > max){
+                    max = dy[j];
+                }
+            }
+            dy[i] = max + arr.get(i).h;
+            answer = Math.max(answer, dy[i]);
         }
-
-        return dy[n];
+        return answer;
     }
 
     public static void main(String[] args) {
         Main T = new Main();
         Scanner kb = new Scanner(System.in);
         int n = kb.nextInt();
-        dy = new int[n+1];
-        System.out.println(solution(n));
+        List<Brick> arr = new ArrayList<>();
+        dy = new int[n];
+
+        for( int i = 0 ; i < n; i++ ){
+            int a = kb.nextInt();
+            int b = kb.nextInt();
+            int c = kb.nextInt();
+            arr.add(new Brick(a,b,c));
+        }
+        System.out.println(solution(arr));
     }
 }
