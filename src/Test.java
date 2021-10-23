08d45589bc59154b81e89e1d94677430e05682b0
jinho <g1063114@naver.com>
2021-10-23 12:40:31 +0900

    Dijkstra

--- src/Test.java
+++ src/Test.java
@@ -1,28 +1,69 @@
 import java.util.*;
 
-class Main{
-    public char solution(int n, String s){
-        char answer = ' ';
-        HashMap<Character, Integer> temp = new HashMap<>();
+class Edge implements Comparable<Edge>{
 
-        for( char x : s.toCharArray() ){
-            temp.put(x, temp.getOrDefault(x,0) + 1);
-        }
-        int max = 0;
-        for( char key : temp.keySet() ){
-            if(temp.get(key) > max ){
-                max = temp.get(key);
-                answer = key;
+    public int vertex;
+    public int cost;
+
+    public Edge(int vertex, int cost) {
+        this.vertex = vertex;
+        this.cost = cost;
+    }
+
+    @Override
+    public int compareTo(Edge o) {
+        return this.cost - o.cost;
+    }
+}
+
+class Main{
+    static int n,m;
+    static List<List<Edge>> graph;
+    static int[] dis;
+
+    public void solution(int n){
+        PriorityQueue<Edge> temp = new PriorityQueue<>();
+        temp.offer(new Edge(n,0));
+        dis[n] = 0;
+
+        while(!temp.isEmpty()){
+            Edge tmp = temp.poll();
+            int nV = tmp.vertex;
+            int nC = tmp.cost;
+            if(nC > dis[nV]) continue;
+            for( Edge e : graph.get(nV) ){
+                if( dis[e.vertex] > nC + e.cost ){
+                    dis[e.vertex] = nC + e.cost;
+                    temp.offer(new Edge(e.vertex, nC + e.cost));
+                }
             }
         }
-        return answer;
     }
 
     public static void main(String[] args){
         Main T = new Main();
         Scanner kb = new Scanner(System.in);
-        int n = kb.nextInt();
-        String s = kb.next();
-        System.out.print(T.solution(n,s));
+        n = kb.nextInt();
+        m = kb.nextInt();
+        graph = new ArrayList<List<Edge>>();
+        for(int i = 0 ; i < n; i++ ){
+            graph.add(new ArrayList<Edge>());
+        }
+        dis = new int[n+1];
+        Arrays.fill(dis, Integer.MAX_VALUE);
+        for( int i = 0 ; i < m; i++ ){
+            int a = kb.nextInt();
+            int b = kb.nextInt();
+            int c = kb.nextInt();
+            graph.get(a).add(new Edge(b,c));
+        }
+        T.solution(1);
+        for( int i = 2; i <= n; i++ ){
+            if(dis[i] != Integer.MAX_VALUE){
+                System.out.println(i + " : " + dis[i]);
+            }else{
+                System.out.println(i + " : impossible");
+            }
+        }
     }
 }
