7a283186c0ccd5adcbd1914654517de4cc3520e3
jinho <g1063114@naver.com>
2021-10-26 17:00:26 +0900

    프로그래머스 - 모의고사

--- src/Test.java
+++ src/Test.java
@@ -1,30 +1,37 @@
 import java.util.*;
 
-class Main{
-    public int solution(int n, int[] lost, int[] reserve) {
-        int answer = 0;
-        for (int i = 0; i < lost.length; i++) {
-            for (int j = 0; j < reserve.length; j++) {
-                if (lost[i] == reserve[j]) {
-                    lost[i] = -1;
-                    reserve[j] = -1;
-                    answer++;
-                    break;
-                }
+class Solution {
+    public int[] solution(int[] answers) {
+        int[] chk = {0,0,0};
+        int[][] temp = {{1,2,3,4,5},{2,1,2,3,2,4,2,5},{3,3,1,1,2,2,4,4,5,5}};
+        List<Integer> list = new ArrayList<>();
+
+        for( int i = 0 ; i < answers.length; i++ ){
+            if( temp[0][i%5] == answers[i] ){
+                chk[0]++;
+            }
+            if( temp[1][i%8] == answers[i] ){
+                chk[1]++;
+            }
+            if( temp[2][i%10] == answers[i] ){
+                chk[2]++;
+            }
+        }
+        Arrays.sort(chk);
+        int max = chk[2];
+
+        for( int i = 0 ; i < chk.length; i++ ){
+            if( max == chk[i] ){
+                list.add(i+1);
             }
         }
 
-        for (int i = 0; i < lost.length; i++) {
-            for (int j = 0; j < reserve.length; j++) {
-                if (lost[i] == reserve[j] - 1 || lost[i] == reserve[j] + 1) {
-                    answer++;
-                    reserve[j] = -1;
-                    break;
-                }
-            }
+        int[] answer = new int[list.size()];
+
+        for( int i = 0 ; i < answer.length; i++ ){
+            answer[i] = list.get(i);
         }
 
-        return n - lost.length + answer;
+        return answer;
     }
-
 }
