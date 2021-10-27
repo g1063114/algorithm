e37335b93156b43af5d456a74a3f57fe253ef8f3
jinho <g1063114@naver.com>
2021-10-27 21:19:36 +0900

    프로그래머스 - 약수의 개수와 덧셈

--- src/Test.java
+++ src/Test.java
@@ -1,36 +1,21 @@
 import java.util.*;
 
 class Solution {
-    public int[] solution(int[] answers) {
-        int[] chk = {0,0,0};
-        int max = 0;
-        int[][] temp = {{1,2,3,4,5},{2,1,2,3,2,4,2,5},{3,3,1,1,2,2,4,4,5,5}};
-        List<Integer> list = new ArrayList<>();
+    public int solution(int left, int right) {
+        int answer = 0;
 
-        for( int i = 0 ; i < answers.length; i++ ){
-            if( temp[0][i%5] == answers[i] ){
-                chk[0]++;
+        for( int i = left; i <= right; i++ ){
+            int temp = 0;
+            for( int j = 1 ; j <= i; j++){
+                if( i % j == 0){
+                    temp++;
+                }
             }
-            if( temp[1][i%8] == answers[i] ){
-                chk[1]++;
+            if( temp % 2 == 0){
+                answer += i;
+            }else{
+                answer -= i;
             }
-            if( temp[2][i%10] == answers[i] ){
-                chk[2]++;
-            }
-        }
-        Arrays.sort(chk);
-        max = chk[2];
-
-        for( int i = 0 ; i < chk.length; i++ ){
-            if( max == chk[i] ){
-                list.add(i+1);
-            }
-        }
-
-        int[] answer = new int[list.size()];
-
-        for( int i = 0 ; i < answer.length; i++ ){
-            answer[i] = list.get(i);
         }
 
         return answer;
