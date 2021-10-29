2e190fd4985f90860d8b661f090832f39092d62d
jinho <g1063114@naver.com>
2021-10-29 23:09:50 +0900

    프로그래머스 - 완주하지 못한 선수

--- src/Test.java
+++ src/Test.java
@@ -1,19 +1,23 @@
 import java.util.*;
 
 class Solution {
-    public int[] solution(int[] array, int[][] commands) {
-        int[] answer = {};
-        List<Integer> temp = new ArrayList<>();
-        answer = new int[commands.length];
+    public String solution(String[] participant, String[] completion) {
+        String answer = "";
+        HashMap<String, Integer> temp = new HashMap<>();
 
-        for( int i = 0 ; i < commands.length; i++ ){
-            for( int j = commands[i][0]-1; j <= commands[i][1] - 1; j++ ){
-                temp.add(array[j]);
-            }
-            Collections.sort(temp);
-            answer[i] = temp.get(commands[i][2]-1);
-            temp.clear();
+        for( int i = 0 ; i < participant.length; i++ ){
+            temp.put(participant[i], temp.getOrDefault(participant[i],0)+1);
         }
-        return answer;
+
+        for( int i = 0 ; i < completion.length; i++ ){
+            temp.put(completion[i], temp.getOrDefault(completion[i],0) -1);
+        }
+
+        for(String key : temp.keySet()){
+            if(temp.get(key) == 1){
+                return key;
+            }
+        }
+        return "";
     }
 }
