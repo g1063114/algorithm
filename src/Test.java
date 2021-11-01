2bca96e3e218a1acb1b1e88ffcf7f08ea1c3d2ac
jinho <g1063114@naver.com>
2021-11-01 13:59:33 +0900

    프로그래머스 - 로또의 최고 순위와 최저 순위

--- src/Test.java
+++ src/Test.java
@@ -1,23 +1,38 @@
-import java.util.*;
-
 class Solution {
-    public int solution(int[] numbers) {
-        int answer = 0;
-        HashMap<Integer, Integer> temp = new HashMap<>();
-
-        for( int i = 0 ; i < 10; i++ ){
-            temp.put(i,1);
+    public int rank(int num){
+        switch(num){
+            case 2:
+                return 5;
+            case 3:
+                return 4;
+            case 4:
+                return 3;
+            case 5:
+                return 2;
+            case 6:
+                return 1;
+            default:
+                return 6;
         }
-
-        for( int i = 0 ; i < numbers.length; i++ ){
-            temp.put(numbers[i], temp.getOrDefault(numbers[i],0)-1);
-        }
-
-        for( int key : temp.keySet() ){
-            if( temp.get(key) == 1){
-                answer+=key;
+    }
+    public int[] solution(int[] lottos, int[] win_nums) {
+        int[] answer = new int[2];
+        int zero = 0;
+        int cnt = 0;
+        for( int i = 0 ; i < lottos.length; i++ ){
+            if( lottos[i] == 0 ){
+                zero++;
+            }
+            for( int j = 0 ; j < win_nums.length; j++ ){
+                if( lottos[i] == win_nums[j] ){
+                    cnt++;
+                }
             }
         }
+
+        answer[0] = rank(cnt+zero);
+        answer[1] = rank(cnt);
+
         return answer;
     }
 }
