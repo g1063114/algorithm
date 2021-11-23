91aaec7d059c456a59c4b0c211f7612c6392c4c3
jinho <g1063114@naver.com>
2021-11-23 14:41:10 +0900

    프로그래머스 점프와 순간이동

--- src/Test.java
+++ src/Test.java
@@ -1,50 +1,25 @@
 import java.util.*;
 
 class Solution {
-    public int solution(String dirs) {
+    public int solution(int n) {
         int answer = 0;
-        int curX = 0, curY = 0;
-        HashSet<String> temp = new HashSet<>();
 
-        for( int i = 0 ; i < dirs.length(); i++ ){
-            String path = "";
-            int nextX = curX;
-            int nextY = curY;
-
-            if(dirs.charAt(i) == 'U'){
-                nextY++;
-                path += curX;
-                path += curY;
-                path += nextX;
-                path += nextY;
-            }else if(dirs.charAt(i) == 'D'){
-                nextY--;
-                path += nextX;
-                path += nextY;
-                path += curX;
-                path += curY;
-            }else if(dirs.charAt(i) == 'L'){
-                nextX--;
-                path += curX;
-                path += curY;
-                path += nextX;
-                path += nextY;
+        while(n!=0){
+            if( n % 2 == 0 ){
+                n /= 2;
             }else{
-                nextX++;
-                path += nextX;
-                path += nextY;
-                path += curX;
-                path += curY;
+                n--;
+                answer++;
             }
-            if( nextX < -5 || nextX > 5 || nextY < -5 || nextY > 5){
-                continue;
-            }
-            temp.add(path);
-            curX = nextX;
-            curY = nextY;
         }
-        answer = temp.size();
         return answer;
+    }
+
+
+    public static void main(String[] args) {
+        Solution S = new Solution();
+
+        System.out.print(S.solution(5000));
     }
 
 }
