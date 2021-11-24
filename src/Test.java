69992274fb5e2131ce2b62fc53f047b7f174c4fa
jinho <g1063114@naver.com>
2021-11-24 17:02:25 +0900

    프로그래머스 구명보트

--- src/Test.java
+++ src/Test.java
@@ -1,16 +1,16 @@
 import java.util.*;
 
 class Solution {
-    public int solution(int n) {
+    public int solution(int[] people, int limit) {
         int answer = 0;
+        int min = 0;
+        Arrays.sort(people);
 
-        while(n!=0){
-            if( n % 2 == 0 ){
-                n /= 2;
-            }else{
-                n--;
-                answer++;
+        for( int max = people.length-1; min <= max; max--){
+            if( people[min] + people[max] <= limit ){
+                min++;
             }
+            answer++;
         }
         return answer;
     }
@@ -19,7 +19,12 @@ class Solution {
     public static void main(String[] args) {
         Solution S = new Solution();
 
-        System.out.print(S.solution(5000));
+        int[] temp = new int[4];
+        temp[0] = 70;
+        temp[1] = 50;
+        temp[2] = 80;
+        temp[3] = 50;
+        System.out.print(S.solution(temp,100));
     }
 
 }
