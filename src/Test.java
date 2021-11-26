6605bed480734210cca8b6c6ba59bda0250e416e
jinho <g1063114@naver.com>
2021-11-26 13:25:21 +0900

    프로그래머스 피로도

--- src/Test.java
+++ src/Test.java
@@ -1,50 +1,43 @@
 import java.util.*;
 
 class Solution {
-    public int[] solution(int n, String[] words) {
-        int[] answer = new int[2];
-        List<String> temp = new ArrayList<>();
-        int num = 0;
-        int seq = 0;
-        boolean flag = true;
-
-        for( int i = 0; i < words.length; i++ ){
-            if(temp.contains(words[i])){
-                num = i % n + 1;
-                seq = i / n + 1;
-                flag = false;
-                break;
-            }
-            temp.add(words[i]);
-
-            if( words[i-1].charAt(words[i-1].length()-1) != words[i].charAt(0) ){
-                num = i % n + 1;
-                seq = i / n + 1;
-                flag = false;
-                break;
-            }
-        }
-        answer[0] = num;
-        answer[1] = seq;
-        if( flag ){
-            return new int[]{0,0};
-        }
+    static int answer=0;
+    public int solution(int k, int[][] dungeons) {
+        DFS(k,0,new boolean[dungeons.length],dungeons);
         return answer;
     }
 
+    public void DFS(int k, int count, boolean[] visit, int[][] dungeons){
+        if( count >= visit.length ){
+            answer = count;
+            return;
+        }
+
+        for( int i = 0 ; i < dungeons.length; i++ ){
+            if( visit[i] ){
+                continue;
+            }
+            if( k < dungeons[i][0]) {
+                answer = Math.max(count,answer);
+                continue;
+            }
+            visit[i] = true;
+            DFS(k-dungeons[i][1],count+1,visit,dungeons);
+            visit[i] = false;
+        }
+    }
 
     public static void main(String[] args) {
         Solution S = new Solution();
 
-        String[] temp = new String[7];
-        temp[0] = "hello";
-        temp[1] = "one";
-        temp[2] = "even";
-        temp[3] = "never";
-        temp[4] = "now";
-        temp[5] = "world";
-        temp[6] = "draw";
-        System.out.print(S.solution(2,temp));
+        int[][] temp = new int[3][2];
+        temp[0][0] = 80;
+        temp[0][1] = 20;
+        temp[1][0] = 50;
+        temp[1][1] = 40;
+        temp[2][0] = 30;
+        temp[2][1] = 10;
+        System.out.print(S.solution(80,temp));
     }
 
 }
