ec2fb762e4039d60f902f9ad0e533c94ab537a5b
jinho <g1063114@naver.com>
2021-11-26 16:20:24 +0900

    프로그래머스 큰수만들기

--- src/Test.java
+++ src/Test.java
@@ -1,43 +1,29 @@
 import java.util.*;
 
 class Solution {
-    static int answer=0;
-    public int solution(int k, int[][] dungeons) {
-        DFS(k,0,new boolean[dungeons.length],dungeons);
-        return answer;
-    }
-
-    public void DFS(int k, int count, boolean[] visit, int[][] dungeons){
-        if( count >= visit.length ){
-            answer = count;
-            return;
-        }
-
-        for( int i = 0 ; i < dungeons.length; i++ ){
-            if( visit[i] ){
-                continue;
+    public String solution(String number, int k) {
+        String answer = "";
+        StringBuilder temp = new StringBuilder();
+        int index = 0;
+        int max = Integer.MIN_VALUE;
+        for( int i = 0 ; i < number.length() - k; i++ ){
+            max = 0;
+            for(int j = index; j < k+i; j++ ){
+                if(max < number.charAt(j)-'0'){
+                    max = number.charAt(j)-'0';
+                    index = j+1;
+                }
             }
-            if( k < dungeons[i][0]) {
-                answer = Math.max(count,answer);
-                continue;
-            }
-            visit[i] = true;
-            DFS(k-dungeons[i][1],count+1,visit,dungeons);
-            visit[i] = false;
+            temp.append(max);
         }
+        return temp.toString();
     }
 
     public static void main(String[] args) {
         Solution S = new Solution();
 
-        int[][] temp = new int[3][2];
-        temp[0][0] = 80;
-        temp[0][1] = 20;
-        temp[1][0] = 50;
-        temp[1][1] = 40;
-        temp[2][0] = 30;
-        temp[2][1] = 10;
-        System.out.print(S.solution(80,temp));
+        String number = "4177252841";
+        System.out.print(S.solution(number,4));
     }
 
 }
