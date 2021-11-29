659a7ed7cfed58384dbfff610a4d794692926201
jinho <g1063114@naver.com>
2021-11-29 15:39:32 +0900

    프로그래머스 위장

--- src/Test.java
+++ src/Test.java
@@ -1,47 +1,29 @@
 import java.util.*;
 
 class Solution {
-    public int solution(int bridge_length, int weight, int[] truck_weights) {
-        int answer = 0;
-        Queue<Integer> queue = new LinkedList<>();
+    public int solution(String[][] clothes) {
+        int answer = 1;
+        HashMap<String, Integer> map = new HashMap<>();
 
-        int sum_weight = 0;
-
-        for( int i = 0 ; i < truck_weights.length; i++ ){
-            int truck = truck_weights[i];
-
-            while(true){
-                if(queue.isEmpty()){
-                    queue.add(truck);
-                    sum_weight += truck;
-                    answer++;
-                    break;
-                }else if( queue.size() == bridge_length ){
-                    sum_weight -= queue.poll();
-                }else{
-                    if( sum_weight + truck <= weight ){
-                        queue.add(truck);
-                        sum_weight += truck;
-                        answer++;
-                         break;
-                    }else{
-                        queue.add(0);
-                        answer++;
-                    }
-                }
-            }
+        for( int i = 0; i < clothes.length; i++ ){
+            map.put(clothes[i][1],map.getOrDefault(clothes[i][1],0)+1);
         }
-        return answer + bridge_length;
+
+        if( map.size() > 1 ){
+            for( String key : map.keySet() ){
+                answer *= map.get(key)+1;
+            }
+        }else{
+            answer = clothes.length;
+        }
+        return answer-1;
     }
 
     public static void main(String[] args) {
         Solution S = new Solution();
-        int[] temp = new int[4];
-        temp[0] = 7;
-        temp[1] = 4;
-        temp[2] = 5;
-        temp[3] = 6;
-        System.out.print(S.solution(2,10, temp));
+        String[][] temp = {{"yellowhat", "headgear"}, {"bluesunglasses", "eyewear"}, {"green_turban", "headgear"}};
+
+        System.out.print(S.solution(temp));
     }
 
 }
