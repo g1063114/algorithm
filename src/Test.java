4bfa70c23da7e12edd14b7b51225a2cbc3e2c189
jinho <g1063114@naver.com>
2021-11-29 20:54:58 +0900

    프로그래머스 프린터

--- src/Test.java
+++ src/Test.java
@@ -1,45 +1,55 @@
 import java.util.*;
 
 class Solution {
-    public int[] solution(String[] genre, int[] plays) {
-        List<Integer> temp = new ArrayList<>();
-        int[] answer = {};
-        HashMap<String,Integer> map = new HashMap<>();
+    class Pair{
+        public int idx;
+        public int val;
 
-        for( int i = 0; i < genre.length; i++ ){
-            map.put(genre[i],map.getOrDefault(genre[i],0)+plays[i]);
+        public Pair(int idx, int val) {
+            this.idx = idx;
+            this.val = val;
         }
-        List<String> keyList = new ArrayList<>(map.keySet());
-        Collections.sort(keyList, ((o1, o2) -> map.get(o2).compareTo(map.get(o1))));
+    }
 
-        for( String key : map.keySet() ){
-            HashMap<Integer,Integer> intMap = new HashMap<>();
-            for( int i = 0 ; i < genre.length; i++ ){
-                if(key.equals(genre[i])){
-                    intMap.put(i,plays[i]);
+    public int solution(int[] properties, int location) {
+        int answer = 0;
+        Queue<Pair> queue = new LinkedList<>();
+
+        for( int i = 0 ; i < properties.length; i++ ){
+            queue.add(new Pair(i,properties[i]));
+        }
+
+        while(!queue.isEmpty()){
+            int cur = queue.peek().val;
+            boolean flag = false;
+            for( Pair p : queue ){
+                if(p.val > cur){
+                    flag = true;
+                    break;
                 }
             }
-            List<Integer> intKeyList = new ArrayList<>(intMap.keySet());
-            Collections.sort(intKeyList, (o1, o2) -> intMap.get(o2).compareTo(intMap.get(o1)));
-            for( int x : intKeyList ){
-                temp.add(x);
+            if( flag ){
+                Pair temp = queue.poll();
+                queue.add(temp);
+            }else{
+                answer++;
+                Pair temp = queue.poll();
+                if( temp.idx == location ){
+                    return answer;
+                }
             }
         }
 
-        answer = new int[temp.size()];
 
-        for(int i = 0 ; i < temp.size(); i++ ){
-            answer[i] = temp.get(i);
-        }
         return answer;
     }
 
     public static void main(String[] args) {
         Solution S = new Solution();
-        String[] genre = {"classic","pop","classic","classic","pop"};
-        int[] plays = {500,600,150,800,2500};
+        int[] properties = {2,1,3,2};
+        int location = 2;
 
-        System.out.print(S.solution(genre,plays));
+        System.out.print(S.solution(properties,location));
     }
 
 }
