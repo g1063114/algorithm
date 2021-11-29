5577ef2f9319960b974fd7b4b9f50283255bfc96
jinho <g1063114@naver.com>
2021-11-29 14:31:58 +0900

    프로그래머스 다리를 지나는 트럭

--- src/Test.java
+++ src/Test.java
@@ -1,32 +1,47 @@
 import java.util.*;
 
 class Solution {
-    public int[] solution(int brown, int yellow) {
-        int[] answer = new int[2];
-        int area = brown + yellow;
+    public int solution(int bridge_length, int weight, int[] truck_weights) {
+        int answer = 0;
+        Queue<Integer> queue = new LinkedList<>();
 
-        for( int i = 1 ; i <= area; i++ ){
-            int row = i;
-            int col = area / row;
+        int sum_weight = 0;
 
-            if( row > col ){
-                continue;
-            }
+        for( int i = 0 ; i < truck_weights.length; i++ ){
+            int truck = truck_weights[i];
 
-            if( (row - 2) * (col - 2) == yellow ){
-                answer[0] = col;
-                answer[1] = row;
-
-                return answer;
+            while(true){
+                if(queue.isEmpty()){
+                    queue.add(truck);
+                    sum_weight += truck;
+                    answer++;
+                    break;
+                }else if( queue.size() == bridge_length ){
+                    sum_weight -= queue.poll();
+                }else{
+                    if( sum_weight + truck <= weight ){
+                        queue.add(truck);
+                        sum_weight += truck;
+                        answer++;
+                         break;
+                    }else{
+                        queue.add(0);
+                        answer++;
+                    }
+                }
             }
         }
-        return answer;
+        return answer + bridge_length;
     }
 
     public static void main(String[] args) {
         Solution S = new Solution();
-
-        System.out.print(S.solution(10,2));
+        int[] temp = new int[4];
+        temp[0] = 7;
+        temp[1] = 4;
+        temp[2] = 5;
+        temp[3] = 6;
+        System.out.print(S.solution(2,10, temp));
     }
 
 }
