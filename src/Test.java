5c59ba2cd123dada9eb6412c974acebea0b407f5
jinho <g1063114@naver.com>
2021-11-29 17:46:15 +0900

    프로그래머스 베스트 앨범

--- src/Test.java
+++ src/Test.java
@@ -1,29 +1,45 @@
 import java.util.*;
 
 class Solution {
-    public int solution(String[][] clothes) {
-        int answer = 1;
-        HashMap<String, Integer> map = new HashMap<>();
+    public int[] solution(String[] genre, int[] plays) {
+        List<Integer> temp = new ArrayList<>();
+        int[] answer = {};
+        HashMap<String,Integer> map = new HashMap<>();
 
-        for( int i = 0; i < clothes.length; i++ ){
-            map.put(clothes[i][1],map.getOrDefault(clothes[i][1],0)+1);
+        for( int i = 0; i < genre.length; i++ ){
+            map.put(genre[i],map.getOrDefault(genre[i],0)+plays[i]);
         }
+        List<String> keyList = new ArrayList<>(map.keySet());
+        Collections.sort(keyList, ((o1, o2) -> map.get(o2).compareTo(map.get(o1))));
 
-        if( map.size() > 1 ){
-            for( String key : map.keySet() ){
-                answer *= map.get(key)+1;
+        for( String key : map.keySet() ){
+            HashMap<Integer,Integer> intMap = new HashMap<>();
+            for( int i = 0 ; i < genre.length; i++ ){
+                if(key.equals(genre[i])){
+                    intMap.put(i,plays[i]);
+                }
             }
-        }else{
-            answer = clothes.length;
+            List<Integer> intKeyList = new ArrayList<>(intMap.keySet());
+            Collections.sort(intKeyList, (o1, o2) -> intMap.get(o2).compareTo(intMap.get(o1)));
+            for( int x : intKeyList ){
+                temp.add(x);
+            }
         }
-        return answer-1;
+
+        answer = new int[temp.size()];
+
+        for(int i = 0 ; i < temp.size(); i++ ){
+            answer[i] = temp.get(i);
+        }
+        return answer;
     }
 
     public static void main(String[] args) {
         Solution S = new Solution();
-        String[][] temp = {{"yellowhat", "headgear"}, {"bluesunglasses", "eyewear"}, {"green_turban", "headgear"}};
+        String[] genre = {"classic","pop","classic","classic","pop"};
+        int[] plays = {500,600,150,800,2500};
 
-        System.out.print(S.solution(temp));
+        System.out.print(S.solution(genre,plays));
     }
 
 }
