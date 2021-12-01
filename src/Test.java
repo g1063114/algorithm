27257f5a5c0ed926610ce65d1d1f6ec33d874ed1
jinho <g1063114@naver.com>
2021-12-01 17:12:41 +0900

    프로그래머스 게임 맵 최단거리

--- src/Test.java
+++ src/Test.java
@@ -1,38 +1,55 @@
 import java.util.*;
 
 class Solution {
+    class Pos{
+        public int x;
+        public int y;
+        public int cost;
 
-    public int solution(int n, int a, int b) {
-        int answer = 1;
-        int left = 0 ;
-        int right = 0;
-
-        if( a > b ){
-            left = b;
-            right = a;
-        }else{
-            left = a;
-            right = b;
+        public Pos(int x, int y, int cost) {
+            this.x = x;
+            this.y = y;
+            this.cost = cost;
         }
+    }
+    static int answer = 0;
+    Queue<Pos> queue = new LinkedList<>();
+    static int[] dx = {-1,0,1,0};
+    static int[] dy = {0,-1,0,1};
+    static int n,m;
+    public int solution(int[][] maps) {
+        n = maps.length;
+        m = maps[0].length;
 
-        while(true){
-            if( left % 2 !=0 && right - left == 1 ){
-                break;
-            }
-            left = (left+1)/2;
-            right = (right+1)/2;
-            answer++;
-        }
+        queue.add((new Pos(0,0,1)));
+        maps[0][0] = 0;
+        answer = BFS(0,0,maps);
         return answer;
+    }
+
+    public int BFS(int x, int y, int[][] map){
+        while(!queue.isEmpty()){
+            Pos temp = queue.poll();
+            if( temp.x == m-1 && temp.y == n-1 ){
+                return temp.cost;
+            }
+            for( int i = 0 ; i < 4; i++ ){
+                int nx = temp.x + dx[i];
+                int ny = temp.y + dy[i];
+
+                if( nx >= 0 && nx <m && ny >=0 && ny <n && map[ny][nx] != 0 ){
+                    map[ny][nx] = 0;
+                    queue.add(new Pos(nx,ny, temp.cost+1));
+                }
+            }
+        }
+        return -1;
     }
 
     public static void main(String[] args) {
         Solution S = new Solution();
-        int n = 8;
-        int a = 4;
-        int b = 7;
-
-        System.out.print(S.solution(n,a,b));
+        int[][] temp = {{1,0,1,1,1},{1,0,1,0,1},{1,0,1,1,1},{1,1,1,0,1},{0,0,0,0,1}};
+        System.out.print(S.solution(temp));
     }
 
 }
