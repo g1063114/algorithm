081ba6ddeb353b1f589b6438435379a4e89c5beb
jinho <g1063114@naver.com>
2021-12-02 17:47:16 +0900

    프로그래머스 테스트..

--- src/Test.java
+++ src/Test.java
@@ -1,35 +1,86 @@
 import java.util.*;
 
 class Solution {
+    class Pos{
+        public int x;
+        public int y;
+        public int cost;
+
+        public Pos(int x, int y, int cost) {
+            this.x = x;
+            this.y = y;
+            this.cost = cost;
+        }
+    }
+    static int[] dx = {-1,0,1,0};
+    static int[] dy = {0,-1,0,1};
+    static boolean[][][] chk;
     static int answer;
-    public int solution(int n, int[][] computers) {
-        answer = 0;
-        boolean[] chk = new boolean[n];
-        for( int i = 0 ; i < n; i++ ){
-            if(!chk[i]){
-                DFS(computers, i, chk);
-                answer++;
+    static Queue<Pos> queue = new LinkedList<>();
+
+    public int solution(int[][] board, int c) {
+        chk = new boolean[board.length][board[0].length][2];
+        for( int i = 0 ; i < board.length; i++ ){
+            for( int j = 0 ; j < board[i].length; j++ ){
+                if( board[i][j] == 2 ){
+                    chk[i][j][0] = true;
+                    queue.add(new Pos(j,i,0));
+                }
             }
         }
+        BFS(board,c,0,chk);
         return answer;
     }
 
-    public boolean[] DFS(int[][] computers, int idx, boolean[] chk){
-        chk[idx] = true;
-
-        for(int i = 0 ; i < computers.length; i++ ){
-            if( idx != i && computers[idx][i] == 1 && chk[i] == false ){
-                chk = DFS(computers, i, chk);
+    public void BFS(int[][] board, int c, int sum, boolean[][][] chk){
+        int desX = 0, desY = 0;
+        for( int i = 0 ; i < board.length; i++ ){
+            for( int j = 0 ; j < board[0].length; j++ ){
+                if( board[i][j] == 3 ){
+                    desX = j;
+                    desY = i;
+                }
             }
         }
-        return chk;
+
+        while(!queue.isEmpty()){
+            Pos temp = queue.poll();
+            int cost = temp.cost;
+            int min = Integer.MAX_VALUE;
+
+            if( temp.x == desX && temp.y == desY ) {
+                if( min > cost ){
+                    min = cost;
+                    answer = min;
+                }
+            }
+            for( int i = 0 ; i < 4; i++ ){
+                int nx = temp.x + dx[i];
+                int ny = temp.y + dy[i];
+
+                if( nx >= 0 && nx < board[0].length && ny >= 0 && ny < board.length && chk[ny][nx][0] != true ){
+                        if(board[ny][nx] == 0 || chk[ny][nx][1] == true){
+                            queue.add(new Pos(nx,ny, cost+1));
+                            chk[ny][nx][0] = true;
+                            temp.cost -= 1;
+                        }else if(board[ny][nx] == 1){
+                            temp.cost = cost + 1 + c;
+                            queue.add(new Pos(nx,ny, temp.cost));
+                            chk[ny][nx][1] = true;
+                            temp.cost -= c;
+                        }else{
+                            queue.add(new Pos(nx,ny, cost+1));
+                        }
+                }
+            }
+        }
     }
 
     public static void main(String[] args) {
         Solution S = new Solution();
-        int n = 3;
-        int[][] computers = {{1,1,0},{1,1,0},{0,0,1}};
-        System.out.print(S.solution(n,computers));
+        int[][] board = {{0,0,0,0,2,0,0,0,0,0},{0,0,1,1,1,1,1,0,0,0},{0,0,1,1,1,1,1,1,0,0},{0,0,1,1,1,1,1,0,1,0},{0,0,1,1,1,1,1,0,0,0},{0,0,0,0,3,0,0,0,1,0}};
+        int c = 2;
+        System.out.print(S.solution(board, c));
     }
 
 }
