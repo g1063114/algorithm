1062a3aa34d7659ab14a648290c32b36ad33cf47
jinho <g1063114@naver.com>
2021-12-03 21:21:01 +0900

    프로그래머스 소수찾기

--- src/Test.java
+++ src/Test.java
@@ -1,86 +1,55 @@
 import java.util.*;
 
 class Solution {
-    class Pos{
-        public int x;
-        public int y;
-        public int cost;
-
-        public Pos(int x, int y, int cost) {
-            this.x = x;
-            this.y = y;
-            this.cost = cost;
-        }
-    }
-    static int[] dx = {-1,0,1,0};
-    static int[] dy = {0,-1,0,1};
-    static boolean[][][] chk;
+    static boolean[] chk = new boolean[7];
     static int answer;
-    static Queue<Pos> queue = new LinkedList<>();
+    static List<Integer> list = new ArrayList<>();
 
-    public int solution(int[][] board, int c) {
-        chk = new boolean[board.length][board[0].length][2];
-        for( int i = 0 ; i < board.length; i++ ){
-            for( int j = 0 ; j < board[i].length; j++ ){
-                if( board[i][j] == 2 ){
-                    chk[i][j][0] = true;
-                    queue.add(new Pos(j,i,0));
-                }
-            }
+    public int solution(String number) {
+        String temp = "";
+
+        for( int i = 0 ; i < number.length(); i++ ){
+            rec(number,temp,i);
         }
-        BFS(board,c,0,chk);
+
+        for( int x : list ){
+            isPrime(x);
+        }
         return answer;
     }
 
-    public void BFS(int[][] board, int c, int sum, boolean[][][] chk){
-        int desX = 0, desY = 0;
-        for( int i = 0 ; i < board.length; i++ ){
-            for( int j = 0 ; j < board[0].length; j++ ){
-                if( board[i][j] == 3 ){
-                    desX = j;
-                    desY = i;
-                }
+    public static void rec(String n, String temp, int len){
+        if( temp.length() == len ){
+            if(!list.contains(Integer.parseInt(temp))){
+                list.add(Integer.parseInt(temp));
+                return;
             }
         }
 
-        while(!queue.isEmpty()){
-            Pos temp = queue.poll();
-            int cost = temp.cost;
-            int min = Integer.MAX_VALUE;
-
-            if( temp.x == desX && temp.y == desY ) {
-                if( min > cost ){
-                    min = cost;
-                    answer = min;
-                }
-            }
-            for( int i = 0 ; i < 4; i++ ){
-                int nx = temp.x + dx[i];
-                int ny = temp.y + dy[i];
-
-                if( nx >= 0 && nx < board[0].length && ny >= 0 && ny < board.length && chk[ny][nx][0] != true ){
-                        if(board[ny][nx] == 0 || chk[ny][nx][1] == true){
-                            queue.add(new Pos(nx,ny, cost+1));
-                            chk[ny][nx][0] = true;
-                            temp.cost -= 1;
-                        }else if(board[ny][nx] == 1){
-                            temp.cost = cost + 1 + c;
-                            queue.add(new Pos(nx,ny, temp.cost));
-                            chk[ny][nx][1] = true;
-                            temp.cost -= c;
-                        }else{
-                            queue.add(new Pos(nx,ny, cost+1));
-                        }
-                }
-            }
+        for( int i = 0; i < n.length(); i++ ){
+            if(chk[i]) continue;
+            temp += n.charAt(i);
+            chk[i] = true;
+            rec(n, temp, len);
+            chk[i] = false;
+            temp = temp.substring(0,temp.length()-1);
         }
+    }
+
+    public static void isPrime(int n){
+        if( n == 0 ) return;
+        if( n == 1 ) return;
+
+        for( int i = 2; i < Math.sqrt(n); i++ ){
+            if( n % i == 0) return;
+        }
+        answer++;
     }
 
     public static void main(String[] args) {
         Solution S = new Solution();
-        int[][] board = {{0,0,0,0,2,0,0,0,0,0},{0,0,1,1,1,1,1,0,0,0},{0,0,1,1,1,1,1,1,0,0},{0,0,1,1,1,1,1,0,1,0},{0,0,1,1,1,1,1,0,0,0},{0,0,0,0,3,0,0,0,1,0}};
-        int c = 2;
-        System.out.print(S.solution(board, c));
+        String number = "011";
+        System.out.print(S.solution(number));
     }
 
 }
