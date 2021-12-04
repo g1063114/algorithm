d235cc757eb241f7e04a9e1c347c027afc5c929e
jinho <g1063114@naver.com>
2021-12-04 19:49:07 +0900

    프로그래머스 소수

--- src/Test.java
+++ src/Test.java
@@ -1,33 +1,45 @@
 import java.util.*;
 
 class Solution {
-    public int solution(int[] scoville, int k) {
+    boolean[] prime;
+    public int solution(int n, int m) {
         int answer = 0;
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
 
-        for( int x : scoville ){
-            pq.add(x);
-        }
+        makePrime(m);
+        int sum = 0;
+        int min = Integer.MAX_VALUE;
 
-        while(pq.peek() < k){
-            if(pq.size() < 2){
-                return -1;
+        for( int i = n; i <= m; i++){
+            if(!prime[i]){
+                sum+=i;
+                if( min == Integer.MAX_VALUE ){
+                    min = i;
+                }
             }
-            int temp1 = pq.poll();
-            int temp2 = pq.poll();
-
-            int newScoville = temp1 + temp2 * 2;
-            pq.add(newScoville);
-            answer++;
         }
-        return answer;
+        if( sum == 0 ){
+            return -1;
+        }else{
+            return sum;
+        }
+    }
+    public void makePrime(int m){
+        prime = new boolean[m+1];
+        prime[0] = true;
+        prime[1] = true;
+
+        for( int i = 2; i <= Math.sqrt(m); i++){
+            for( int j = i*i; j <m; j+=i ){
+                prime[j] = true;
+            }
+        }
     }
 
     public static void main(String[] args) {
         Solution S = new Solution();
-        int[] scoville = {1,2,3,9,10,12};
-        int k = 7;
-        System.out.print(S.solution(scoville,k));
+        int n = 60;
+        int n2 = 100;
+        System.out.print(S.solution(n,n2));
     }
 
 }
