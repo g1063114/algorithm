f3ad8b37b9793eb177c1904e12d505662ae0d7df
jinho <g1063114@naver.com>
2021-12-05 11:28:25 +0900

    백준 acm호텔

--- src/Test.java
+++ src/Test.java
@@ -1,45 +1,21 @@
 import java.util.*;
 
 class Solution {
-    boolean[] prime;
-    public int solution(int n, int m) {
-        int answer = 0;
-
-        makePrime(m);
-        int sum = 0;
-        int min = Integer.MAX_VALUE;
-
-        for( int i = n; i <= m; i++){
-            if(!prime[i]){
-                sum+=i;
-                if( min == Integer.MAX_VALUE ){
-                    min = i;
-                }
-            }
-        }
-        if( sum == 0 ){
-            return -1;
-        }else{
-            return sum;
-        }
-    }
-    public void makePrime(int m){
-        prime = new boolean[m+1];
-        prime[0] = true;
-        prime[1] = true;
-
-        for( int i = 2; i <= Math.sqrt(m); i++){
-            for( int j = i*i; j <m; j+=i ){
-                prime[j] = true;
-            }
-        }
-    }
-
     public static void main(String[] args) {
-        Solution S = new Solution();
-        int n = 60;
-        int n2 = 100;
-        System.out.print(S.solution(n,n2));
-    }
+        Scanner sc = new Scanner(System.in);
 
+        int t = sc.nextInt();
+
+        for( int i = 0 ; i < t; i++ ){
+            int h = sc.nextInt();
+            int w = sc.nextInt();
+            int n = sc.nextInt();
+
+            if( h % n == 0 ){
+                System.out.println(h*100 + (n / h));
+            }else{
+                System.out.println((n % h)*100 + (n / h)+1);
+            }
+        }
+    }
 }
