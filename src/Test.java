953be0b2020145cd5591dc502fa9fdd3ac8eea00
jinho <g1063114@naver.com>
2021-12-06 11:17:32 +0900

    백준 소인수분해

--- src/Test.java
+++ src/Test.java
@@ -4,18 +4,16 @@ class Solution {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int t = sc.nextInt();
+        int n = sc.nextInt();
 
-        for( int i = 0 ; i < t; i++ ){
-            int h = sc.nextInt();
-            int w = sc.nextInt();
-            int n = sc.nextInt();
-
-            if( h % n == 0 ){
-                System.out.println(h*100 + (n / h));
-            }else{
-                System.out.println((n % h)*100 + (n / h)+1);
+        for( int i = 2; i <= Math.sqrt(n); i++ ){
+            while( n % i == 0 ){
+                System.out.println(i);
+                n /= i;
             }
+        }
+        if( n != 1 ){
+            System.out.println(n);
         }
     }
 }
