4cbe93e91c2c33fdd11beb39c5f9fd7bedd017a1
jinho <g1063114@naver.com>
2021-12-06 11:56:04 +0900

    백준 베르트랑 공준

--- src/Test.java
+++ src/Test.java
@@ -4,16 +4,20 @@ class Solution {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int n = sc.nextInt();
-        int m = sc.nextInt();
-        boolean[] prime = new boolean[m+1];
+        boolean[] prime = new boolean[246913];
 
-        checkPrime(prime);
+        while(true){
+            int n = sc.nextInt();
+            if( n == 0 ) break;
+            int cnt = 0;
+            checkPrime(prime);
 
-        for( int i = n; i <= m; i++ ){
-            if( !prime[i] ){
-                System.out.println(i);
+            for( int i = n ; i <= 2*n; i++ ){
+                if( !prime[i] ){
+                    cnt++;
+                }
             }
+            System.out.println("cnt = " + cnt);
         }
     }
 
