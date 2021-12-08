552610b4620b17bf847bc813f2a0987f67d9f95c
jinho <g1063114@naver.com>
2021-12-08 10:45:24 +0900

    백준 직사각형에서의 탈출

--- src/Test.java
+++ src/Test.java
@@ -4,14 +4,14 @@ class Solution {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int x = sc.nextInt();
-        int y = sc.nextInt();
-        int w = sc.nextInt();
-        int h = sc.nextInt();
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
 
-        int minX = Math.min(x-0, w-x);
-        int minY = Math.min(y-0, h-y);
-
-        System.out.println(Math.min(minX,minY));
+        if( Math.pow(c,2) == Math.pow(a,2) + Math.pow(b,2) ){
+            System.out.println("right");
+        }else{
+            System.out.println("wrong");
+        }
     }
 }
